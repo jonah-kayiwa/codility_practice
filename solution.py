f2ef271a3a98c4f401765a306dc403a08ad882c0
jonah-kayiwa <152336281+jonah-kayiwa@users.noreply.github.com>
2024-03-11 16:34:27 +0300

    first commit

--- solution.py
+++ solution.py
@@ -1,3 +1,11 @@
-def solution(A, d):
-    return solution(range(-1,000..1,000))
-for 
+#define the solution in which range its going to play
+def final_balance(A, D):
+    return ('range(-1,000..1,000)')
+
+#wriet a fomular for computing account balance
+account_balance =('available_balance - transaction_amount')
+transaction_amount =()
+sorted
+#write a function to state out the transaction calculation
+
+#codility, can make you think of trying farming.wueehh
