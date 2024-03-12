59bc31e90273fa5095cc53afcb8f9e342ce5a490
jonah-kayiwa <152336281+jonah-kayiwa@users.noreply.github.com>
2024-03-12 10:35:46 +0300

    final commit

--- solution.py
+++ solution.py
@@ -1,17 +1,27 @@
-#define the solution for initialising the variable
-def final_balance(A, D):
+def solution(A, D):
+    monthly_fee = 5
     balance = 0
-    card_payments = [0] * 12
-   if i in range (len(A)):
-       Amount = A[i]
+    cards = {}
 
+#lets use the ZIP to loop through 
+    for i, date in zip(A, D):
+        month = int(date.split('-') [1])
+        if i >= 0:
+            balance += i
 
+#change the negative balance            
+        else:
+            balance += i
+        if month in cards:
+            cards[month].append(i)
+        else:
+            cards[month] = [i]
 
+#create a loop through the months
+for month in range(1, 13):
+    if month in cards  and len(cards[month]) >= 3 and sum (cards[nonth]) >= -100: 
+        balance += monthly_fees *(len(cards[month]) - 3)
+    else:
+        balance -= monthly_fees                         
 
-#wriet a fomular for computing account balance
-account_balance =('available_balance - transaction_amount')
-transaction_amount =()
-sorted
-#write a function to state out the transaction calculation
-
-#codility, can make you think of trying farming.wueehh
+#run some tests to see if code runs
